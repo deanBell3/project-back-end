ceaf299070a5d80a8200ed9d813d329feb0e2f27
Dean Bell <belljohan09@gmail.com>
2018-09-11 17:21:10 +0000

    yuh

--- api/articles.js
+++ api/articles.js
@@ -5,25 +5,25 @@ router.get('/articles', (req,res)=>{
     db.select('*').from('articles').orderBy('created_the', 'desc').then(data=>res.send(data)).catch(err=>res.send(err))
 })
 router.post('/articles', (req,res)=>{
-    const {headline, main_picture, picture_reference}= req.body
-    db('articles').insert({headline, main_picture, picture_reference}).then(()=>{
+    const {head, main, reference}= req.body
+    db('articles').insert({head, main, reference}).then(()=>{
         res.send('Article posted successfully')
     }).catch(err=>res.send(err))
 })
 
 router.get('/articles/:id',(req,res)=>{
-   const article_id= req.params.id;
-   db.select('*').from('articles').where({article_id}).then(data=>res.send(data)).catch(err=>res.send(err))
+   const id= req.params.id;
+   db.select('*').from('articles').where({id}).then(data=>res.send(data)).catch(err=>res.send(err))
 })
 router.get('/articles/:id/paragraphs',(req,res)=>{
-    const article= req.params.id;
-    db.select('*').from('paragraph').where({article}).then(data=>res.send(data)).catch(err=>res.send(err))
+    const article_id= req.params.id;
+    db.select('*').from('paragraph').where({article_id}).then(data=>res.send(data)).catch(err=>res.send(err))
 })
 
 router.post('/articles/:id/paragraphs',(req,res)=>{
     const{content}=req.body;
     const{id}= req.params;
-    db('paragraph').insert({content,article:id}).then(()=>res.send('paragraph added successfully')).catch(err=>res.send(err))
+    db('paragraph').insert({content,article_id:id}).then(()=>res.send('paragraph added successfully')).catch(err=>res.send(err))
 })
 // TEST ROUTES FOR INCORPORATION OF NEW STYLES
 router.get('/article',(req,res)=>{
