ee6c64f7b3a19392f426812d962dcb9ed5e0ff7e
Dean Bell <belljohan09@gmail.com>
2018-12-18 13:14:11 +0000

    project-back-end

--- data/data.js
+++ data/data.js
@@ -1,12 +1,12 @@
 const knex= require('knex')({
     client:'pg',
     connection:{
-        connectionString: process.env.DATABASE_URL,
-        ssl:true
-       /* host:'localhost',
+        /*connectionString: process.env.DATABASE_URL,
+        ssl:true*/
+       host:'localhost',
         database:'elevate',
         user:'ubuntu',
-        password:'dean'*/
+        password:'dean'
         
     }
 })
